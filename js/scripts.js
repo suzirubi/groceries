a874063469131742a60acc2723e0f28c8c6db3c7
Ryan Peterson and Suzi Rubino <suzi@>
2016-10-24 15:22:26 -0700

    add functions in javascript to sort and display list after hiding submittal form

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,20 @@ $(document).ready(function(){
   $("form#grocery-list").submit(function(event){
 
     var items = ["item1", "item2", "item3", "item4"];
-    var list = [];
+    var groceryList = [];
 
     items.forEach(function(item){
       var userInput = $("input#" + item).val();
-      list.push(userInput.toUpperCase());
+      groceryList.push(userInput.toUpperCase());
+      groceryList.sort();
+      //var addFood = groceryList.shift();
+      //$("." + item).text(addFood);
     });
 
-    list.sort();
+    groceryList.forEach(function(grocery){
+        $("ul").append("<li>" + grocery + "</li>");
+    });
+    $("form#grocery-list").hide();
     event.preventDefault();
   });
 });
