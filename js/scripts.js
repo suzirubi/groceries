f19722c036e6090d9f723ad25cdd48439a411f68
Steve Burton and Suzi Rubino <steve@>
2016-10-25 12:06:54 -0700

    add for script to replace forEach

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,31 @@
 $(document).ready(function(){
   $("form#grocery-list").submit(function(event){
     $("ul").text("");
-    var items = ["item1", "item2", "item3", "item4"];
-    var groceryList = [];
 
-    items.forEach(function(item){
-      var userInput = $("input#" + item).val();
-      groceryList.push(userInput.toUpperCase());
-      groceryList.sort();
+    var items = [];
+    // var groceryList = [];
 
-      //var addFood = groceryList.shift();
-      //$("." + item).text(addFood);
-    });
+    for (var item = 0; item < items.length; item += 1) {
+      var  item1 = $("input#item1").val();
+      var  item2 = $("input#item2").val();
+      var  item3 = $("input#item3").val();
+      var  item4 = $("input#item4").val();
+      items.push(item);
+      console.log(items);
+    }
 
-    groceryList.forEach(function(grocery){
-        $("ul").append("<li>" + grocery + "</li>");
-    });
-    //$("form#grocery-list").hide();
+
+    // items.forEach(function(item){
+    //   var userInput = $("input#" + item).val();
+    //   groceryList.push(userInput.toUpperCase());
+    //   groceryList.sort();
+    //
+    // });
+
+    // groceryList.forEach(function(grocery){
+    //     $("ul").append("<li>" + grocery + "</li>");
+    // });
+
     event.preventDefault();
   });
 });
